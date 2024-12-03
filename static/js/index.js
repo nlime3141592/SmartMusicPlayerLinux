eaa987a2f341f8c716ae08b3dab16279bcb4faf7
limeLimelimeLime <nlime3141592@naver.com>
2024-12-04 02:27:41 +0900

    feat: link to cam page

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,9 @@
 document.getElementById("btn_refresh_page").onclick = async () =>
 {
-    // NOTE: This block is intentionally left blank.
-    // TODO: Implement URI; POST /refresh
+    window.location.reload()
+}
+
+document.getElementById("btn_cam_page").onclick = async () =>
+{
+    window.location.href = "/cam"
 }
