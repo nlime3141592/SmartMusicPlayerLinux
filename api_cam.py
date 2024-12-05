425e531a114ad3a9897983972b06537457c497d0
YelloGreenLime <nlime3141592@naver.com>
2024-12-05 23:16:03 +0900

    fix: Delete live stream

--- api_cam.py
+++ api_cam.py
@@ -13,18 +13,3 @@ def init(websv, proc_cntx):
     @websv.route("/cam", methods=["GET"])
     def cam():
         return render_template("./cam.html")
-
-    @websv.route("/cam/live/image", methods=["POST"])
-    def cam_live_image():
-        proc_cntx.shmem.write("cam;live;image;")
-
-        shape = proc_cntx.shmem.read()
-        dtype = proc_cntx.shmem.read()
-        img_bytes = proc_cntx.shmem.read()
-
-        frame = np.frombuffer(img_bytes, dtype=dtype).reshape(shape)
-
-        return Response(__generate_frames(frame), mimetype="multipart/x-mixed-replace; boundary=frame")
-
-    def __generate_frames(frame):
-        yield (b"--frame\n"b"Content-Type: image/jpeg\n\n" + frame + b"\n\n")
--- cam_handler.py
+++ cam_handler.py
@@ -2,6 +2,8 @@ import cv2
 import cam
 import proc
 
+import logmodule as logger
+
 __logger_name = "CamService"
 
 def init():
@@ -10,26 +12,11 @@ def init():
 def update_always():
     cam.update_always()
 
-def __update_cam_live(service_tokenizer):
-    service_type = service_tokenizer.read()
-
-    if service_type == "image":
-        img_bytes = cam.generate_frame()
-
-        shape = img_bytes.shape
-        dtype = str(img_bytes.dtype)
-
-        proc.proc_ctx.shmem.write(shape)
-        proc.proc_ctx.shmem.write(dtype)
-        proc.proc_ctx.shmem.write(img_bytes.tobytes())
-    else:
-        logger.print_log("Invalid service requested, rejects.", logger_name=__logger_name)
-
 def update(service_tokenizer):
     service_type = service_tokenizer.read()
 
     if service_type == "live":
-        __update_cam_live(service_tokenizer)
+        pass
     else:
         logger.print_log("Invalid service requested, rejects.", logger_name=__logger_name)
 
