1f764249ad45cb444e4b4372c85f191070257314
YelloGreenLime <nlime3141592@naver.com>
2024-12-05 23:44:56 +0900

    fix: Change execution directory

--- process_context.py
+++ process_context.py
@@ -1,8 +1,16 @@
+from flask import Flask
 from shared_memory import SharedMemory
+
+import multiprocessing as mp
+import numpy as np
 
 class ProcessContext:
     name: str
     shmem: SharedMemory
+    cam_bytes: bytes
+    cam_lock: mp.Lock
+    current_dir: str
+    websv: Flask
     handler_init: [..., any]
     handler_update_always: [..., any]
     handler_update: [..., any]
@@ -11,6 +19,10 @@ class ProcessContext:
     def __init__(self):
         self.name = ""
         self.shmem = None
+        self.cam_bytes = None
+        self.cam_lock = None
+        self.current_dir = None
+        self.websv = None
         self.handler_init = None
         self.handler_update_always = None
         self.handler_update = None
